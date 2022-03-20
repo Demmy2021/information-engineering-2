d45f27f8c18450169624d6a3b28f95ca411f9292
Demmy2021 <azeecam@gmail.com>
2022-03-20 14:41:42 +0100

    Assignment 1

---
+++ lab01/Student.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+struct Student {
+    std::string name;
+    std::string surname;
+    std::vector<float> grades;
+
+};
+
+
---
+++ lab01/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Student.hpp>
+#include <vector>
+#include <string>
+
+
+
+
+int main()
+{
+    //std::cout << "Hello World!" << std::endl;
+
+
+    system("pause");
+
+    return 0;
+}
